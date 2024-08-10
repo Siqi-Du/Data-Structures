b26a33e9b432c0e1a74b136e8991ac1e440bb81c
Siqi-Du <siqidu005@gmail.com>
2024-08-09 21:09:52 -0700

    fix: comment

--- Binary-Search/src/BinarySearch.java
+++ Binary-Search/src/BinarySearch.java
@@ -10,7 +10,7 @@ public class BinarySearch {
         int r = n -1 ;
 
         while(l <= r){
-            int mid = (l+r)/2;
+            int mid = (l+r)/2; // (l+(r-l)/2) -> 防整形溢出如果l和r都很大
             if(arr[mid].compareTo(target) == 0)
                 return mid;
             if(arr[mid].compareTo(target) < 0)
